b76a07fc869f39878ec1bd90dd0be964440ec58f
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-05-04 14:22:22 -0700

    App.jsx complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import "./App.css";
+import Demo from "./components/Demo";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div class="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
