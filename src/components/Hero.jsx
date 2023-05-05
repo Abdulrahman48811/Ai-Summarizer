df29db7841a129f9755182ed418d2db74a20ed2a
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-05-05 12:57:59 -0700

    completed header/hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
 
-export default Hero
+        <button
+          type="button"
+          onClick={() =>
+            window.open("https://github.com/Abdulrahman48811/Ai-Summarizer")
+          }
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient ">OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className="desc">
+        Summize, an open-source article summarizer that converts long articles
+        into simple and succinct summaries, might help you simplify your
+        reading.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
