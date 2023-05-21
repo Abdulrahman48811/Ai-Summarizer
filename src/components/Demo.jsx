78e7d6c7e23c363941e543637443f17214059c4b
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-05-21 12:40:02 -0700

    Save Articles

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -11,18 +11,29 @@ const Demo = () => {
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const { data } = await getSummary({ articleUrl: article.url });
 
-    if(data?.summary)  {
-        const newArticle = { ...article, summary: data.summary };
-        const updatedAllArticles = [newArticle, ...allArticles];
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
 
-        setArticle(newArticle);
-        setAllArticles(updatedAllArticles);
-        console.log(newArticle);
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
   };
   return (
